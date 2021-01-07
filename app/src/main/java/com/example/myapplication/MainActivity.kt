5cd7ec77e3f86acb821261ce0c528f1867f5a03e
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-01-07 10:44:15 +0900

    feat(MainActivity): add MyViewModel dependency

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val vm: MyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActibityDebugPrint", vm.getCounter().toString())
     }
 }
