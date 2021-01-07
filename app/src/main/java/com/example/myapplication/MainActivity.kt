467e2646d8773ede2208e445b4d0dab8190009ee
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-01-07 13:19:09 +0900

    feat(MainActivity): implement counter app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActibityDebugPrint", vm.getCounter().toString())
+        val textView = findViewById<TextView>(R.id.text)
+        vm.getCountLiveData().observe(this, Observer {
+            textView.text = it.toString()
+            Log.d("MainActivityDebugPrint", vm.getCount().toString())
+        })
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            vm.increment()
+        }
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            vm.decrement()
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MyViewModel.kt
+++ app/src/main/java/com/example/myapplication/MyViewModel.kt
@@ -1,19 +1,27 @@
 package com.example.myapplication
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
     private var count: Int = 0
+    private var countLiveData = MutableLiveData<Int>(count)
 
-    fun getCounter(): Int {
+    fun getCount():Int{
         return count
+    }
+
+    fun getCountLiveData(): MutableLiveData<Int>{
+        return countLiveData
     }
 
     fun increment() {
         count++
+        countLiveData.postValue(count)
     }
 
     fun decrement() {
-        count++
+        count--
+        countLiveData.postValue(count)
     }
 }
